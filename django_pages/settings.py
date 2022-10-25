0e3c8b09869164d5707ec48e55c9208453982fec
chan ka wing <chankawing@38-f9-d3-b2-cf-8d.wlan.lsu.edu>
2022-10-25 16:09:44 -0500

    configuring for heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tby&9tok43zo#7%t1biv(p7*(kwymmz$w@18e&ntkp_jxe_e*c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
